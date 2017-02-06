b7cb752532358a6899bc70299055ca2fe44b55af
piotr.minkowski <piotr.minkowski@gmail.com>
2017-02-06 16:14:51 +0100

    Initial commit

---
+++ src/main/java/pl/piomin/microservices/person/Application.java
@@ -0,0 +1,14 @@
+package pl.piomin.microservices.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
