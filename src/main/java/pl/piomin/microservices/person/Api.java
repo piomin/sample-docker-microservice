8fadb9bc08b1a731d0ad090f32d03ccf3cf3db63
piomin <piotr.minkowski@gmail.com>
2021-03-06 23:39:31 +0100

    add jib + update

--- src/main/java/pl/piomin/microservices/person/Api.java
+++ src/main/java/pl/piomin/microservices/person/Api.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/persons")
 public class Api {
 
 	protected Logger logger = Logger.getLogger(Api.class.getName());
@@ -22,14 +24,14 @@ public class Api {
 		persons.add(new Person(1, "Tomasz", "Janowski", 25));
 		persons.add(new Person(1, "Alina", "Iksińska", 54));
 	}
-	
-	@RequestMapping("/person")
+
+	@GetMapping
 	public List<Person> findAll() {
 		logger.info("Api.findAll()");
 		return persons;
 	}
 	
-	@RequestMapping("/person/{id}")
+	@GetMapping("/{id}")
 	public Person findById(@PathVariable("id") Integer id) {
 		logger.info(String.format("Api.findById(%d)", id));
 		return persons.stream().filter(p -> (p.getId().intValue() == id)).findAny().get();
