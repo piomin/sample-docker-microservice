b2ca95cae1f9e21199649bad26da71d1a0028aa1
piomin <piotr.minkowski@gmail.com>
2023-11-06 16:08:58 +0100

    switching to java 21 and bellsoft openjdk alpine images

---
+++ spring-microservice/src/test/java/pl/piomin/microservices/spring/MyAppTests.java
@@ -0,0 +1,13 @@
+package pl.piomin.microservices.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+public class MyAppTests {
+
+    @Test
+    void example() {
+
+    }
+}
