b9ca3962025c35d9abc947859a3410944b4b732b
piomin <piotr.minkowski@gmail.com>
2026-02-12 13:31:14 +0100

    fix tests

--- spring-microservice/src/test/java/pl/piomin/microservices/spring/ApiTests.java
+++ spring-microservice/src/test/java/pl/piomin/microservices/spring/ApiTests.java
@@ -3,10 +3,12 @@ package pl.piomin.microservices.spring;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 public class ApiTests {
 
     @Autowired
